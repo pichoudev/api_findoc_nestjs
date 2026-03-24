466ba73b419c4b658e5d9e2698f4de1ff4627cea
pichoudev <devsteph237@proton.me>
2026-03-24 12:51:24 +0100

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,24 +4,16 @@
 // Prisma 6+ — avec url obligatoire pour migrate
 // ============================================================
 
-// import 'dotenv/config'
-// import { defineConfig } from 'prisma/config'
+import 'dotenv/config'
+import { defineConfig } from 'prisma/config'
 
-// export default defineConfig({
-//   schema: 'prisma/schema.prisma',
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
 
-//   datasource: {
-//     url: process.env.DATABASE_URL!,
-//   },
-// })
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+})
 
 // prisma.config.js
-require('dotenv/config');
-const { defineConfig, env } = require('prisma/config');
 
-module.exports = defineConfig({
-  schema: 'prisma/schema.prisma',
-  datasource: {
-    url: env('DIRECT_URL'),
-  },
-});
