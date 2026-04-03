b8dc749c255b27a74f9d5b1361889ec575396fbb
pichoudev <devsteph237@proton.me>
2026-04-03 13:40:57 +0200

    fix: mise a jour de la route update user v2

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -117,12 +117,6 @@ export class ReportsService {
           include: this.reportInclude,
         });
 
-        // Mettre à jour le reportType du bac avec la valeur du signalement
-        await tx.bin.update({
-          where: { id: bacId },
-          data: { reportType: reportType as ReportStatus }, // Cast pour éviter l'erreur de type
-        });
-
         // Mettre à jour le statusReport du bac avec son propre reportType converti en ReportStatus
         await tx.bin.update({
           where: { id: bacId },
